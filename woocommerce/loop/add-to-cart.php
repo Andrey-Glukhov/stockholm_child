ce7fd8111d5446765be21e227be09b1079020172
ANdy <agi2000@mail.ru>
2020-12-09 23:44:23 +0300

    change shop buttons

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -26,15 +26,15 @@ if ( ! $product->is_in_stock() ) : ?>
 	<?php	echo sprintf('<div class="add-to-card-name-child">%s</div>', esc_html($product->get_title()));
  else :
 	echo apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
-		sprintf( '<div class="add-to-cart-button-outer"><div class="add-to-cart-button-inner"><div class="add-to-cart-button-inner2"><a href="%s" data-quantity="%s" class="qbutton add-to-cart-button %s" %s>%s</a></div><div class="add-to-card-name-child">%s</div><div class="add-to-card-price-child">%s</div></div></div>',
+		sprintf( '<div class="add-to-cart-button-outer"><div class="add-to-cart-button-inner"><div class="add-to-card-name-child">%s</div><div class="add-to-card-price-child">%s</div></div></div><div class="add-to-cart-button-inner2"><a href="%s" data-quantity="%s" class="qbutton add-to-cart-button %s" %s>%s</a></div>',
+			esc_html($product->get_title()),
+			$product->get_price_html(),
 			esc_url( $product->add_to_cart_url() ),
 			esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
 			esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
 			isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
 			//esc_html( $product->add_to_cart_text() ),
-			'',
-			esc_html($product->get_title()),
-			$product->get_price_html()
+			''
 		),
 		$product, $args );
 
